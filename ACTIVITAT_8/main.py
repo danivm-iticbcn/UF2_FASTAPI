ef366072e5e131ecf48b54ce2124c9ca35e1df61
danivm-iticbcn <2023_daniel.vallespin@iticbcn.cat>
2024-11-11 19:04:14 +0100

    post fet i actualitzacio del readme

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"bienvenida":"Bienvenido al inicio de esta API"}
+
+
+class Alumno(BaseModel):
+    nombre : str
+    edad : int
+    localidad : str | None = None
+
+
+@app.post("/alumno")
+async def inrtoducirAlumno(alumno: Alumno):
+    return {"Alumno": alumno}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # UF2_FASTAPI
+
+## Daniel Vallespin Mellado
+
+### Activitat 8
+
+#### Get
+<img src="readme_img/get.png"/>
+
+#### Post
+<img src="readme_img/post.png"/>
