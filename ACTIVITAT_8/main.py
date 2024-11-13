9ecf4ea9a25fabaf253729fac0a1a92dd44cd944
danivm-iticbcn <2023_daniel.vallespin@iticbcn.cat>
2024-11-13 20:21:57 +0100

    get amb status_code 404

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -3,17 +3,20 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", status_code=404) #Status_code para cambiar el codigo que devolvemos
 async def root():
     return {"bienvenida":"Bienvenido al inicio de esta API"}
 
-
+#BaseModel para el post
 class Alumno(BaseModel):
     nombre : str
+    id : int
     edad : int
-    localidad : str | None = None
+    localidad : str | None = None   #Campo opcional
+    estudios : str
+    curso : str
 
-
+#Post
 @app.post("/alumno")
 async def inrtoducirAlumno(alumno: Alumno):
     return {"Alumno": alumno}
--- README.md
+++ README.md
@@ -4,8 +4,22 @@
 
 ### Activitat 8
 
-#### Get
+#### Get (swagger)
 <img src="readme_img/get.png"/>
 
-#### Post
+#### Post (swagger)
 <img src="readme_img/post.png"/>
+
+#### Get (postman)
+<img src="readme_img/get_postman.png"/>
+
+#### Post (postman)
+<img src="readme_img/post_postman.png"/>
+
+#### BaseModel amb 6 camps
+<img src="readme_img/baseModel.png"/>
+
+#### Error amb Response Status Code
+<img src="readme_img/error404.png"/>
+
+#### Tractament d'error amb HTTPException
